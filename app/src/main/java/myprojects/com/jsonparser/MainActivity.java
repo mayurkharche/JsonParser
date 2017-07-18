c8dd8b3afd870472b77cb2f6bbc15e15320c9194
Mayur Kharche <mayurkharche1994@gmail.com>
2017-07-18 15:00:32 +0530

    Initial commit

---
+++ app/src/main/java/myprojects/com/jsonparser/MainActivity.java
@@ -0,0 +1,13 @@
+package myprojects.com.jsonparser;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
